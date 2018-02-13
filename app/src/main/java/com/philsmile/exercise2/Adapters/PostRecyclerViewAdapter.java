e412c18e740a818cd8cf0a2d3e7a742f08e4c4dd
VincentMagtalas <joseph.magtalas@gmail.com>
2018-02-13 14:23:49 +0800

    implemented Longclick Listener but without AlertDialog

--- app/src/main/java/com/philsmile/exercise2/Adapters/PostRecyclerViewAdapter.java
+++ app/src/main/java/com/philsmile/exercise2/Adapters/PostRecyclerViewAdapter.java
@@ -5,22 +5,27 @@ package com.philsmile.exercise2.Adapters;
  */
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 /**
  * Created by philsmile on 2/8/2018.
  */
+import com.philsmile.exercise2.MainActivity;
 import com.philsmile.exercise2.R;
 import com.philsmile.exercise2.Classes.DisplayPost;
-
-import java.util.List;
 
 public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerViewAdapter.ViewHolder> {
 
@@ -42,10 +47,9 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
     }
 
     @Override
-    public void onBindViewHolder(PostRecyclerViewAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(PostRecyclerViewAdapter.ViewHolder holder, final int position) {
         final DisplayPost post = postList.get(position);
 
-        Log.i("XOXOAdapter",post.getPostID());
 
         String postid = post.getPostID().toString();
         String username = post.getUserName().toString();
@@ -53,6 +57,16 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
 
         holder.tvName.setText(username);
         holder.tvBody.setText(body);
+
+        holder.cardItem.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                Toast.makeText(context,"need to add to bookmark",Toast.LENGTH_LONG).show();
+
+                return false;
+            }
+        });
 
     }
 
@@ -62,12 +76,13 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder  {
-
+        public CardView cardItem;
         public TextView tvName;
         public TextView tvBody;
 
         public ViewHolder(View view) {
             super(view);
+            cardItem = (CardView) view.findViewById(R.id.cardItem);
             tvName = (TextView) view.findViewById(R.id.tvName);
             tvBody = (TextView) view.findViewById(R.id.tvBody);
 
