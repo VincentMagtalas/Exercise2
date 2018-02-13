a60cc7fb894030ea17ea4be53c9fc5dc7b4cb8fd
VincentMagtalas <joseph.magtalas@gmail.com>
2018-02-13 17:33:55 +0800

    Implemented Search Function

--- app/src/main/java/com/philsmile/exercise2/MainActivity.java
+++ app/src/main/java/com/philsmile/exercise2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.philsmile.exercise2;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -7,10 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -35,12 +42,21 @@ public class MainActivity extends AppCompatActivity {
     String usersURL = "https://jsonplaceholder.typicode.com/users";
 
     ArrayList<DisplayPost> arrayListDisplay = new ArrayList<>();
+    ArrayList<DisplayPost> arrayListDisplaySearch = new ArrayList<>();
     RecyclerView rv;
+
+    private Toolbar mToolbar;
+    private MenuItem mSearchAction;
+    private boolean isSearchOpened = false;
+    private EditText edtSeach;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         rv = (RecyclerView) findViewById(R.id.post_rv);
 
@@ -161,10 +177,102 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(),BookmarkActivity.class);
                 startActivity(intent);
                 return true;
+            case R.id.action_search:
+                handleMenuSearch();
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        mSearchAction = menu.findItem(R.id.action_search);
+        return super.onPrepareOptionsMenu(menu);
+    }
+    protected void handleMenuSearch(){
+        android.support.v7.app.ActionBar action = getSupportActionBar(); //get the actionbar
+
+        if(isSearchOpened){ //test if the search is open
+
+            action.setDisplayShowCustomEnabled(false); //disable a custom view inside the actionbar
+            action.setDisplayShowTitleEnabled(true); //show the title in the action bar
+
+            //hides the keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(edtSeach.getWindowToken(), 0);
+
+            //add the search icon in the action bar
+            //mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_search_category_default));
+
+            isSearchOpened = false;
+        } else { //open the search entry
+
+            action.setDisplayShowCustomEnabled(true); //enable it to display a
+            // custom view in the action bar.
+            action.setCustomView(R.layout.search_bar);//add the custom view
+            action.setDisplayShowTitleEnabled(false); //hide the title
+
+            edtSeach = (EditText)action.getCustomView().findViewById(R.id.edtSearch); //the text editor
+
+            //this is a listener to do a search when the user clicks on search button
+            edtSeach.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                        doSearch(edtSeach.getText().toString());
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+
+            edtSeach.requestFocus();
+
+            //open the keyboard focused in the edtSearch
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(edtSeach, InputMethodManager.SHOW_IMPLICIT);
+
+
+            //add the close icon
+            //mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_close_search));
+
+            isSearchOpened = true;
+        }
+    }
+
+    private void doSearch(String inputSearch) {
+        arrayListDisplaySearch.clear();
+
+        if(inputSearch.equals("")){
+            PostRecyclerViewAdapter postRecyclerViewAdapter = new PostRecyclerViewAdapter(arrayListDisplay,getApplicationContext());
+            rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            rv.setAdapter(postRecyclerViewAdapter);
+        }else{
+            for(int i=0; i<arrayListDisplay.size(); i++){
+                DisplayPost dp = arrayListDisplay.get(i);
+
+                String postID = dp.getPostID().toString();
+                String userID = dp.getUserID().toString();
+                String username = dp.getUserName().toString();
+                String title = dp.getTitle().toString();
+                String body = dp.getBody().toString();
+
+                if(inputSearch.toString().equals(postID.toString())){
+                    arrayListDisplaySearch.add(new DisplayPost(postID,userID,username,title,body));
+                    PostRecyclerViewAdapter postRecyclerViewAdapter = new PostRecyclerViewAdapter(arrayListDisplaySearch,getApplicationContext());
+                    rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                    rv.setAdapter(postRecyclerViewAdapter);
+                }else{
+                    PostRecyclerViewAdapter postRecyclerViewAdapter = new PostRecyclerViewAdapter(arrayListDisplaySearch,getApplicationContext());
+                    rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                    rv.setAdapter(postRecyclerViewAdapter);
+                }
+            }
+        }
+    }
+
+
 
 
 }
